ecc21740e1eb118eb0da0e0736cc872c4c32ca91
Sandeep Meegahapola <smeegahapola5@gmail.com>
2023-09-06 22:15:27 -0500

    loop through my new password string to create password

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const numbers='1234567890'
 const symbols='~!@#$%^&*_+'
 const lowercaseletters='azxcvbnmkljshgdfqwertyuiop'
 const uppercaseletters='AZXCVBNMLKSJHGDFQWERTYUIOP'
-let newpassword=[]
+let newpassword=''
+let password=''
 
 
 // Get references to the #generate element
@@ -18,11 +19,38 @@ function generatePassword(){
   const wantsym=confirm('would you like symbols')
   const wantuppercase=confirm('would you like uppercase')
   const wantlowercase=confirm('would you like lowercase')
+  if(wantnum===true){
+    newpassword += numbers
+  }
+  if(wantsym===true){
+    newpassword+=symbols
+  }
 
-  console.log('this is a message to sandeep!!!!!',passwordlength,wantnum,wantsym,wantuppercase,wantlowercase)
+  if(wantuppercase===true){
+    newpassword +=uppercaseletters
+  }
+
+  if(wantlowercase===true){
+    newpassword +=lowercaseletters
+  }
+  if(!wantnum && !wantsym && !wantuppercase && !wantlowercase){
+    alert('you must select atleast one choice')
+    return null
+  }
+  for(let i=0;i<passwordlength;i++){
+    let chosenpassword=Math.floor(Math.random()* newpassword.length)
+    password +=newpassword.charAt(chosenpassword)
+   
+  console.log(chosenpassword)
+  
+  }
+  return password
+  console.log('this is a message to sandeep!!!!!',passwordlength,wantnum,wantsym,wantuppercase,wantlowercase,
+  newpassword)
 }
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
